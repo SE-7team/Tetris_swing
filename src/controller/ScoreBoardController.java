841416c4082c5f66a96e65182c064fc455ea29ce
momokaP <9451sh@naver.com>
2024-04-25 15:11:16 +0900

    unit test v1

--- src/controller/ScoreBoardController.java
+++ src/controller/ScoreBoardController.java
@@ -61,7 +61,7 @@ public class ScoreBoardController {
         model.initData();
     }
 
-    private void initFocus() {
+    protected void initFocus() {
         // JComponent.WHEN_IN_FOCUSED_WINDOW
         InputMap im = view.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
         // InputMap im = view.getRootPane().getInputMap();
@@ -79,6 +79,14 @@ public class ScoreBoardController {
         });
 
 
+    }
+
+    public Object getView() {
+        return view;
+    }
+
+    public void setView(ScoreBoardView view) {
+        this.view = view;
     }
 
 }
